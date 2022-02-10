417f3f1aebb044f37df40ea7b19314bf9a6639ac
eungyukm <eungyukm@gmail.com>
2022-02-10 10:41:05 +0900

    Addressables 추가

---
+++ Addressables/Assets/Scripts/Manager/BundleManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class BundleManager : MonoBehaviour
+{
+    public AssetLabelReference assetLabelReference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetLocation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetLocation()
+    {
+        Addressables.GetDownloadSizeAsync(assetLabelReference.labelString).Completed +=
+            (handle) =>
+            {
+                Debug.Log("Size : " + handle.Result);
+            };
+    }
+}
